5df362f9d0c1654976682cb189cea338d800c2eb
Arik Alexandrov <arikxl@gmail.com>
2023-06-12 23:28:55 +0300

    clean

--- ex1.js
+++ ex1.js
@@ -1,18 +1,3 @@
-// 1) 1.1- Create a Person class, has 5 attributes - id, name, birthday, parentName, hobby.
-//   1.2 - Create getters / setters for each attribute (getName, setName etc..)
-//   1.3 - Create a class Teacher that extends Person, and adds new attributes:
-//    a) array of professions the teacher is teaching
-//    b) schoolName: string - the school the teacher is teaching
-//    c) startDate: string - a date string when the teacher started teaching
-//  2) create a Student class that extends Person with 3 additional attributes:
-//   a) the classes the student takes (array of strings)
-//   b) startDate: a date string when the student started studying
-//   c) teacher: Teacher - the "educator" teacher of the student (needs to be an instance of the class Teacher)
-// // read at home about public/protected/private functions in Classes
-//   2.1 add to student a public method - getAverage() - that returns the average score of the student
-//   2.2 add a private method getScoreByClass(class: string) that returns the score per provided argument class
-//   2.3 - protected method in Person - getIdPlusName - that returns the string: '{ID}_{NAME}'
-// 3) [Bonus] - Solve the let o = '( [ ) ]' example in leetcode challenge we did in class (isValid string ((())))
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -72,10 +57,7 @@ var Person = /** @class */ (function () {
     return Person;
 }());
 var arik = new Person(1001, "Arik", "16/22/84", "Ra'aya", "Eating");
-// console.log('arik:', arik)
-// console.log(arik.getHobby())
-// arik.setParentName('RAYA')
-// console.log('arik:', arik)
+
 var Teacher = /** @class */ (function (_super) {
     __extends(Teacher, _super);
     function Teacher(id, name, birthday, parentName, hobby, professions, schoolName, startDate) {
@@ -89,12 +71,7 @@ var Teacher = /** @class */ (function (_super) {
 }(Person));
 var yaron = new Teacher(1, "Yaron Biton", "01/01/70", "Saba", "smoking", ["JS", "Algo"], "CA School", "02/03/2001");
 console.log('yaron:', yaron);
-// console.log(yaron.name);
-// console.log(yaron.professions);
-// yaron.name = "YARON";
-// yaron.professions.push("css");
-// console.log(yaron.professions);
-// console.log(yaron.name);
+
 var Student = /** @class */ (function (_super) {
     __extends(Student, _super);
     function Student(id, name, birthday, parentName, hobby, classes, startDate, teacher, grades) {
@@ -123,15 +100,4 @@ var Student = /** @class */ (function (_super) {
     return Student;
 }(Person));
 var ohav = new Student(1, "Ohav Yaron", "10/10/2000", "Aba Ima", "Coding", ["Html", "css", "js"], "12/06/23", yaron, [100, 89, 95]);
-// console.log('ohav:', ohav)
-// console.log(ohav.name);
-// console.log(ohav.classes);
-// console.log(ohav.teacher.name);
-// console.log(ohav.getAverage()); 
-// ohav.name = "OHAV!!!";
-// ohav.classes.push("TS");
-// ohav.teacher.name = "ARIK";
-// console.log(ohav.name);
-// console.log(ohav.classes);
-// console.log(ohav.teacher.name);
-// console.log(ohav.getAverage()); 
+
